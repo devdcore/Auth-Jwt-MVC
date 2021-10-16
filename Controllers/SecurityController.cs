ce62eca5414084273bfea6d27bec346afd15b444
Daniel Valle <danielvalle25@gmail.com>
2021-10-16 08:18:43 -0400

    App
    Get Token Jwt

---
+++ Controllers/SecurityController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Auth_Jwt_MVC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SecurityController : ControllerBase
+    {
+
+        private string GenerateJSONWebToken(string username)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("LlavedeSeguridad"));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim("Issuer", "Shiv"),
+                new Claim("Admin", "true"),
+                new Claim(JwtRegisteredClaimNames.UniqueName, username)
+            };
+
+            var token = new JwtSecurityToken("Shiv",
+                "Shiv",
+                claims,
+                expires: DateTime.Now.AddMinutes(120),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+
+
+        // GET: api/<SecurityController>
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return GenerateJSONWebToken("danielv");
+        //}
+
+        // GET api/<SecurityController>/5
+        [HttpGet]
+        public string Get()
+        {
+            return GenerateJSONWebToken("danielv");
+        }
+
+        // POST api/<SecurityController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<SecurityController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<SecurityController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Controllers/Security_Controller.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auth_Jwt_MVC.Controllers
+{
+    public class Security_Controller : Controller
+    {
+        private string GenerateJSONWebToken(string username)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("LlavedeSeguridad"));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim("Issuer", "Shiv"),
+                new Claim("Admin", "true"),
+                new Claim(JwtRegisteredClaimNames.UniqueName, username)
+            };
+
+            var token = new JwtSecurityToken("Shiv",
+                "Shiv",
+                claims,
+                expires: DateTime.Now.AddMinutes(120),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+
+        // GET: SecurityController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: SecurityController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: SecurityController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SecurityController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SecurityController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: SecurityController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SecurityController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: SecurityController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
